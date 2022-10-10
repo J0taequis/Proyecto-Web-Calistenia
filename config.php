7d51478de02d1e9e99dc1985b5987db80900d03c
Sion Arancibia <sionarancibia@gmail.com>
2022-10-09 22:48:20 -0300

    configphp

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "calistenia";
+$servername = "192.168.4.20";
+$username = "A2022_sarancibia";
+$password = "A2022_sarancibia";
+$dbname = "A2022_sarancibia";
